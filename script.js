925959b751aa1f984ce8782ad9c40626b500c67e
helioramon <helio.alvareng@gmail.com>
2022-06-13 08:42:17 -0300

    creating condition with if and else

---
+++ script.js
@@ -0,0 +1,20 @@
+let age = 20
+
+if ( age >= 18){
+    console.log ("Acesso permitido")
+}else{
+    console.log ("Acesso negado")
+}
+if (age >= 18){
+    console.log ("True")
+}else{
+    console.log ("False")
+}
+
+let birthday = 'dezembro'
+
+if (birthday == 'dezembro' || birthday == 'janeiro'){
+    console.log ("Parabéns")
+}
+
+
